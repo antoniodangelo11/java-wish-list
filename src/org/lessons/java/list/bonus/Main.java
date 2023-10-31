ed51c84ec398e790f7ed8b6a2cf63a7b1b859d93
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-31 03:17:44 +0100

    start bonus

---
+++ src/org/lessons/java/list/bonus/Main.java
@@ -0,0 +1,10 @@
+package org.lessons.java.list.bonus;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+    }
+}
